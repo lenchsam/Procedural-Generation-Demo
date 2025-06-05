26c1ebf0ed1060a6fa7a4ad8091331c4143bed01
Sam <114984800+lenchsam@users.noreply.github.com>
2025-06-05 15:42:31 +0100

    Back to main menu
    menu doesnt work when going back to it

--- Assets/Scripts/CameraSpawning.cs
+++ Assets/Scripts/CameraSpawning.cs
@@ -11,22 +11,23 @@ public class CameraSpawning : MonoBehaviour
         if(mapData.MapSize == new Vector2(20, 20))
         {
             mainCamera.transform.position = spawnPoints[0].position;
-            Debug.Log("Spawning camera at 20x20 map size");
         }
         else if (mapData.MapSize == new Vector2(30, 30))
         {
             mainCamera.transform.position = spawnPoints[1].position;
-            Debug.Log("Spawning camera at 30x30 map size");
         }
         else if(mapData.MapSize == new Vector2(40, 40))
         {
-            Debug.Log("Spawning camera at 40x40 map size");
             mainCamera.transform.position = spawnPoints[2].position;
         }
         else if(mapData.MapSize == new Vector2(50, 50))
         {
             mainCamera.transform.position = spawnPoints[3].position;
-            Debug.Log("Spawning camera at 50x50 map size");
         }
+    }
+    public void newScene(string sceneName)
+    {
+        GameObject sceneManager = GameObject.Find("----SceneManager----");
+        sceneManager.GetComponent<SceneManager>().LoadScene(sceneName);
     }
 }
