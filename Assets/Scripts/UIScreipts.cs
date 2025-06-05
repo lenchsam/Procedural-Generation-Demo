73d31362150c6b4b75e40a7dc7bd0fdb550ed078
Sam <114984800+lenchsam@users.noreply.github.com>
2025-06-05 22:50:06 +0100

    fixed main menu issue

---
+++ Assets/Scripts/UIScreipts.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class UIScreipts : MonoBehaviour
+{
+    public void newScene(string sceneName)
+    {
+        GameObject sceneManager = GameObject.Find("----SceneManager----");
+        sceneManager.GetComponent<SceneManager>().LoadScene(sceneName);
+    }
+}
