3e5f8b1defa0acb01edcb68bc1446a6546363ccd
Sam <114984800+lenchsam@users.noreply.github.com>
2025-07-03 00:19:45 +0100

    points now reset on turn end

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -6,19 +6,31 @@ public class Unit : MonoBehaviour, IAttacking
     public e_Team Team;
     public int DamageRange = 1;
     private int _damage = 50;
-    [SerializeField] private int _movementPoints = 1;
+    [SerializeField] private int _MaxMovementPoints = 1;
+    private int _movementPoints;
 
     private HexGrid _hexGrid;
+
+    private TurnManager _turnManager;
 
     void Awake()
     {
         _hexGrid = FindAnyObjectByType<HexGrid>();
+        _turnManager = FindAnyObjectByType<TurnManager>();
+
+        _turnManager.OnTurnEnd.AddListener(ResetMovementPoints);
+
+        _movementPoints = _MaxMovementPoints;
     }
     public int GetMovementPoints()
     {
         return _movementPoints;
     }
     
+    public void ResetMovementPoints()
+    {
+        _movementPoints = _MaxMovementPoints;
+    }
     public void TakeMovementPoints(int pointsToTake)
     {
         _movementPoints -= pointsToTake;
